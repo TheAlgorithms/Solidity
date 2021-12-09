8fca275fb08e305ce90aa856930d7a9d6527120a
Maxime TFT <mkubik@tftlabs.com>
2021-12-09 14:54:53 +0100

    ⚡ Add subDigit contract

---
+++ Maths/subDigit.sol
@@ -0,0 +1,26 @@
+// Solidity program to demonstrate the subtraction
+pragma solidity 0.6.6;
+
+// In Subtraction, we could have used any value of Integer
+// (int16 to int256) but all the operands must be of the same
+// data types (same bits Integers).
+
+// Creating a contract
+contract subDigit
+{
+	// Initializing the
+	// state variables
+	int16 firstNo = 2 ;
+	int16 secondNo = 10;
+	
+	// Defining a function
+	// to subtract two numbers
+	function Sub() view public returns (int16)
+	{
+		int16 ans = firstNo - secondNo;
+		
+		// Difference amount
+		return ans;
+	}
+
+}
