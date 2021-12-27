63af24b240e22a0fa0e31ac5ee23a1b031225de3
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2021-12-27 20:36:21 +0100

    🐛 fix contract instance for testing

--- Maths/fibonacci/testFibonacci.sol
+++ Maths/fibonacci/testFibonacci.sol
@@ -1,28 +1,26 @@
 pragma solidity ^0.8.0;
 
 import {Fibonacci} from "./fibonacci.sol";
+import {DeployedAddresses} from "truffle/DeployedAddresses.sol";
 
-contract testFibonacci is Fibonacci {
-    function isPointerArrayEquals(uint[] memory _a, uint[] memory _b) public pure returns (bool isGood) {
-        assembly {
-            isGood := eq(_a, _b)
-        }
-    }
+
+contract testFibonacci {
+    Fibonacci fib= Fibonacci= Fibonacci(DeployedAddresses.Fibonacci());
 
     function testGoodFibRecursiveTerm() public pure {
-        require(fibRecursiveTerm(0) == 0, "Fibonacci O-th term is 0.");
-        require(fibRecursiveTerm(1) == 1, "Fibonacci 1-th term is 1.");
-        require(fibRecursiveTerm(2) == 1, "Fibonacci 2-th term is 1.");
-        require(fibRecursiveTerm(3) == 2, "Fibonacci 3-th term is 2.");
-        require(fibRecursiveTerm(4) == 3, "Fibonacci 4-th term is 3.");
-        require(fibRecursiveTerm(5) == 5, "Fibonacci 5-th term is 5.");
-        require(fibRecursiveTerm(6) == 8, "Fibonacci 6-th term is 8.");
-        require(fibRecursiveTerm(7) == 13, "Fibonacci 7-th term is 13.");
-        require(fibRecursiveTerm(8) == 21, "Fibonacci 8-th term is 21.");
-        require(fibRecursiveTerm(9) == 34, "Fibonacci 9-th term is 34.");
-        require(fibRecursiveTerm(10) == 55, "Fibonacci 10-th term is 55.");
-        require(fibRecursiveTerm(17) == 1597, "Fibonacci 17-th term is 1597.");
-        //assert(fibRecursiveTerm(99) == 218922995834555169026); Consume too much gas
+        require(fib.fibRecursiveTerm(0) == 0, "Fibonacci O-th term is 0.");
+        require(fib.fibRecursiveTerm(1) == 1, "Fibonacci 1-th term is 1.");
+        require(fib.fibRecursiveTerm(2) == 1, "Fibonacci 2-th term is 1.");
+        require(fib.fibRecursiveTerm(3) == 2, "Fibonacci 3-th term is 2.");
+        require(fib.fibRecursiveTerm(4) == 3, "Fibonacci 4-th term is 3.");
+        require(fib.fibRecursiveTerm(5) == 5, "Fibonacci 5-th term is 5.");
+        require(fib.fibRecursiveTerm(6) == 8, "Fibonacci 6-th term is 8.");
+        require(fib.fibRecursiveTerm(7) == 13, "Fibonacci 7-th term is 13.");
+        require(fib.fibRecursiveTerm(8) == 21, "Fibonacci 8-th term is 21.");
+        require(fib.fibRecursiveTerm(9) == 34, "Fibonacci 9-th term is 34.");
+        require(fib.fibRecursiveTerm(10) == 55, "Fibonacci 10-th term is 55.");
+        require(fib.fibRecursiveTerm(17) == 1597, "Fibonacci 17-th term is 1597.");
+        //assert(fib.fibRecursiveTerm(99) == 218922995834555169026); Consume too much gas
     }
 
     function testGoodFibRecursive() public pure {
@@ -30,27 +28,27 @@ contract testFibonacci is Fibonacci {
         uint[] memory result;
 
         expected_total[0]= 0;
-        result = fibRecursive(0);
+        result = fib.fibRecursive(0);
         require(expected_total[0] == result[0], "Fibonacci 0-th suite last value is 0.");
 
         expected_total[1]= 1;
-        result = fibRecursive(1);
+        result = fib.fibRecursive(1);
         require(expected_total[1] == result[1], "Fibonacci 1-th suite last value is 1.");
 
         expected_total[2]= 1;
-        result = fibRecursive(2);
+        result = fib.fibRecursive(2);
         require(expected_total[2] == result[2], "Fibonacci 2-th suite last value is 1.");
 
         expected_total[3]= 2;
-        result = fibRecursive(3);
+        result = fib.fibRecursive(3);
         require(expected_total[3] == result[3], "Fibonacci 3-th suite last value is 2.");
 
         expected_total[4]= 3;
-        result = fibRecursive(4);
+        result = fib.fibRecursive(4);
         require(expected_total[4] == result[4], "Fibonacci 4-th suite last value is 3.");
 
         expected_total[5]= 5;
-        result = fibRecursive(5);
+        result = fib.fibRecursive(5);
         require(expected_total[5] == result[5], "Fibonacci 5-th suite last value is 5.");
 
         expected_total[6]= 8;
@@ -59,7 +57,7 @@ contract testFibonacci is Fibonacci {
         expected_total[9]= 34;
         expected_total[10]= 55;
 
-        result = fibRecursive(10);
+        result = fib.fibRecursive(10);
         for (uint i= 0; i <= 10; i++) {
             require(result[i] == expected_total[i],
                 string(abi.encodePacked("Fibonacci expected value for ", i, "-th is ", expected_total[i]))
@@ -72,27 +70,27 @@ contract testFibonacci is Fibonacci {
         uint[] memory result;
 
         expected_total[0]= 0;
-        result = fibMemoization(0);
+        result = fib.fibMemoization(0);
         require(expected_total[0] == result[0], "Fibonacci 0-th suite last value is 0.");
 
         expected_total[1]= 1;
-        result = fibMemoization(1);
+        result = fib.fibMemoization(1);
         require(expected_total[1] == result[1], "Fibonacci 1-th suite last value is 1.");
 
         expected_total[2]= 1;
-        result = fibMemoization(2);
+        result = fib.fibMemoization(2);
         require(expected_total[2] == result[2], "Fibonacci 2-th suite last value is 1.");
 
         expected_total[3]= 2;
-        result = fibMemoization(3);
+        result = fib.fibMemoization(3);
         require(expected_total[3] == result[3], "Fibonacci 3-th suite last value is 2.");
 
         expected_total[4]= 3;
-        result = fibMemoization(4);
+        result = fib.fibMemoization(4);
         require(expected_total[4] == result[4], "Fibonacci 4-th suite last value is 3.");
 
         expected_total[5]= 5;
-        result = fibMemoization(5);
+        result = fib.fibMemoization(5);
         require(expected_total[5] == result[5], "Fibonacci 5-th suite last value is 5.");
 
         expected_total[6]= 8;
@@ -101,14 +99,14 @@ contract testFibonacci is Fibonacci {
         expected_total[9]= 34;
         expected_total[10]= 55;
 
-        result = fibMemoization(10);
+        result = fib.fibMemoization(10);
         for (uint i= 0; i <= 10; i++) {
             require(result[i] == expected_total[i],
                 string(abi.encodePacked("Fibonacci expected value for ", i, "-th is ", expected_total[i]))
             );
         }
 
-        result= fibMemoization(99);
+        result= fib.fibMemoization(99);
         require(result[99] == 218922995834555169026, "Fibonacci 99-th suite last value.");
     }
 }
