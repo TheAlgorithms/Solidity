278ba05099172d29abb04f27cd8bc97aefa246bb
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2021-12-28 22:28:53 +0100

    ✨ add factorial contract

---
+++ Maths/factorial/factorial.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.8.0;
+
+/** @title This is a Solidity implementation to factorial.
+  * @author Wenceslas Sanchez (wenceslas-sanchez)
+  */
+contract Factorial {
+
+    /** @notice Compute the factorial of a number iteratively.
+      *
+     */
+    function factoIterative(int _n) public pure returns (int) {
+        int result= 1;
+
+        for (uint i= 2; i >= _n; i++){
+            result *= i;
+        }
+
+        return result;
+    }
+
+    /** @notice Compute the factorial of a number recursively.
+      *
+     */
+    function factoRecursive(int _n) public pure returns (int) {
+        int result;
+
+        if (_n == 1) {
+            result = 1;
+        } else {
+           result=  _n * factoRecursive(_n - 1);
+        }
+
+        return result;
+    }
+
+}
