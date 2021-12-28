df07d98d17d05a84d482b697d42669ed559b6b2b
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2021-12-28 22:17:09 +0100

    ✨ add Armstrong number contract

---
+++ Maths/armstrongNumber/armstrongNumber.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.8.0;
+
+/** @title This is a Solidity implementation to Armstrong number.
+  * @notice An Armstrong number is number that is equal to the sum of
+  * cubes of its digits.
+  * @author Wenceslas Sanchez (wenceslas-sanchez)
+  */
+contract armstrongNumber {
+    /** @notice Return the digit numbers of an integer.
+      * @dev From https://ethereum.stackexchange.com/questions/6623/can-i-check-the-number-of-digits-in-solidity
+      */
+    function numDigits(int _n) public pure returns (uint) {
+        uint8 digits = 0;
+        while (_n != 0) {
+            _n /= 10;
+            digits++;
+        }
+
+        return digits;
+    }
+
+    /** @notice Function to check whether a number is armstrong or not.
+      * @return `true` if the number is armstrong
+      */
+    function isArmstrongNumber(uint _n) public pure returns (bool) {
+        uint sum= 0;
+        uint saveNum= _n;
+        uint rightNum;
+
+        uint digits= numDigits(int(_n));
+
+        while (saveNum > 0) {
+            rightNum = saveNum % 10;
+            sum += rightNum ** digits;
+            saveNum /= 10;
+        }
+
+        return _n == sum;
+    }
+}
