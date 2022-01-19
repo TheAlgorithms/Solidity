6d038b3e4131636485dc6defa7949b7789ad2385
Wenceslas Sanchez <85337624+wenceslas-sanchez@users.noreply.github.com>
2022-01-19 16:12:40 +0100

    Correct CI fitter (#5)
    * 👷 setup Solidity code formatter
    
    * 👷 change contract .sol location
    
    * 👷 change contract .sol location
    
    * ✏️fix missing end line
    
    * 🐛 delete litter action
    
    * 🎨 prettier reformat
    
    * ✨ re add github litter action

--- Maths/subDigit.sol
+++ Maths/subDigit.sol
@@ -6,19 +6,16 @@ pragma solidity 0.6.6;
 // data types (same bits Integers).
 
 // Creating a contract
-contract subDigit
-{
-	// Initializing the state variables
-	int16 firstNo = 2 ;
-	int16 secondNo = 10;
-	
-	// Defining a function to subtract two numbers
-	function Sub() view public returns (int16)
-	{
-		int16 ans = firstNo - secondNo;
-		
-		// Difference amount
-		return ans;
-	}
+contract subDigit {
+    // Initializing the state variables
+    int16 firstNo = 2;
+    int16 secondNo = 10;
 
+    // Defining a function to subtract two numbers
+    function Sub() public view returns (int16) {
+        int16 ans = firstNo - secondNo;
+
+        // Difference amount
+        return ans;
+    }
 }
--- Maths/sumDigit.sol
+++ Maths/sumDigit.sol
@@ -2,30 +2,26 @@
 pragma solidity 0.6.6;
 
 // Creating a contract
-contract sumDigit
-{
-	// Declaring the state variables
-	uint firstNo;
-	uint secondNo;
+contract sumDigit {
+    // Declaring the state variables
+    uint256 firstNo;
+    uint256 secondNo;
 
-	// Defining the function to set the value of the first variable
-	function firstNoSet(uint x) public
-	{
-		firstNo = x;
-	}
+    // Defining the function to set the value of the first variable
+    function firstNoSet(uint256 x) public {
+        firstNo = x;
+    }
 
-	// Defining the function to set the value of the second variable
-	function secondNoSet(uint y) public
-	{
-		secondNo = y;
-	}
+    // Defining the function to set the value of the second variable
+    function secondNoSet(uint256 y) public {
+        secondNo = y;
+    }
 
-	// Defining the function to add the two variables
-	function add() view public returns (uint)
-	{
-		uint Sum = firstNo + secondNo ;
-		
-		// Sum of two variables
-		return Sum;
-	}
+    // Defining the function to add the two variables
+    function add() public view returns (uint256) {
+        uint256 Sum = firstNo + secondNo;
+
+        // Sum of two variables
+        return Sum;
+    }
 }
