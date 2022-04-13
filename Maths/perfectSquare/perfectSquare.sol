83269883baf94734d130a03d011bf7855d4c2a88
Samuel Rodriguez-Lozano <44348413+samuel-rl@users.noreply.github.com>
2022-04-13 17:04:59 +0200

    feat: add perfect square functions (#8)
    * feat: add perfect square functions
    
    * fix: add missing endline
    
    * Fix Prettier issues
    
    Co-authored-by: David Leal <halfpacho@gmail.com>

---
+++ Maths/perfectSquare/perfectSquare.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+contract PerfectSquare {
+    function sqrt(uint256 x) private pure returns (uint256 y) {
+        uint256 z = (x + 1) / 2;
+        y = x;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
+    }
+
+    function perfectSquare(uint256 _n) public pure returns (bool) {
+        return (sqrt(_n) * sqrt(_n)) == _n;
+    }
+}
