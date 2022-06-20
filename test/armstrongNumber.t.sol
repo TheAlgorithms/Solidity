bfd6b81edd1e933cf3aec30d39783b435be001f7
Perelyn-sama <okohebina@gmail.com>
2022-06-20 23:56:50 +0100

    added test for armstrong number contract

---
+++ test/armstrongNumber.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {ArmstrongNumber} from "../src/Maths/armstrongNumber/armstrongNumber.sol";
+
+contract ArmstrongNumberTest is Test {
+    // Target contract
+    ArmstrongNumber armstrongNumber;
+
+    //  =====   Set up  =====
+    function setUp() public {
+        armstrongNumber = new ArmstrongNumber();
+    }
+
+    //  =====   Functionality tests   =====
+
+    /// @dev Test `isArmstrongNumber`
+
+    /// Should pass if num is an armstrong number
+    function test_IsArmstrongNumber() public {
+        uint256 num = 153;
+
+        bool isTrue = armstrongNumber.isArmstrongNumber(num);
+        assertEq(true, isTrue);
+    }
+
+    /// Should pass if num is not an armstrong number
+    function test_IsNotArmstrongNumber() public {
+        uint256 num = 154;
+
+        bool isFalse = armstrongNumber.isArmstrongNumber(num);
+        assertEq(false, isFalse);
+    }
+}
