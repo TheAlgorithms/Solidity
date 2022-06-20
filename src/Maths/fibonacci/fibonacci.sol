3e58e49c903dacea8cc74eaaf7232c678c49c59c
Perelyn-sama <okohebina@gmail.com>
2022-06-20 22:06:52 +0100

    re wrote fib test

--- src/Maths/fibonacci/fibonacci.sol
+++ src/Maths/fibonacci/fibonacci.sol
@@ -1,16 +1,16 @@
 pragma solidity ^0.8.0;
 
 /** @title This is a Solidity implementation to Fibonacci sequence problem.
-  * @author Wenceslas Sanchez (wenceslas-sanchez)
-  * @dev Binet implementation is missing.
-  */
+ * @author Wenceslas Sanchez (wenceslas-sanchez)
+ * @dev Binet implementation is missing.
+ */
 contract Fibonacci {
     /** @notice Computes the n-th (0-indexed) Fibonacci number using recursion.
-      * @dev This method use a lot of gas to run. Recursive methods are not good ideas.
-      * For instance with RemixIDE, its hard to go further 10th term.
-      */
-    function fibRecursiveTerm(uint _n) public pure returns (uint) {
-        uint result;
+     * @dev This method use a lot of gas to run. Recursive methods are not good ideas.
+     * For instance with RemixIDE, its hard to go further 10th term.
+     */
+    function fibRecursiveTerm(uint256 _n) public pure returns (uint256) {
+        uint256 result;
 
         if (_n < 2) {
             result = _n;
@@ -22,41 +22,40 @@ contract Fibonacci {
     }
 
     /** @notice Computes the first n (0-indexed) Fibonacci numbers using recursion
-      * @dev It is based on fibRecursiveTerm => consume a lot of gas.
-      */
-    function fibRecursive(uint _n) public pure returns (uint[] memory) {
-        uint[] memory result= new uint[](_n + 1);
+     * @dev It is based on fibRecursiveTerm => consume a lot of gas.
+     */
+    function fibRecursive(uint256 _n) public pure returns (uint256[] memory) {
+        uint256[] memory result = new uint256[](_n + 1);
 
-        for (uint i = 0; i < _n + 1; i++) {
-            result[i]= fibRecursiveTerm(i);
+        for (uint256 i = 0; i < _n + 1; i++) {
+            result[i] = fibRecursiveTerm(i);
         }
 
         return result;
     }
 
     /** @notice Computes the first n (0-indexed) Fibonacci numbers using memoization.
-      * @dev Max computable term around 400.
-      */
-    function fibMemoization(uint _n) public pure returns (uint[] memory) {
-        uint cache_size;
+     * @dev Max computable term around 400.
+     */
+    function fibMemoization(uint256 _n) public pure returns (uint256[] memory) {
+        uint256 cache_size;
         if (_n < 2) {
-            cache_size= 2; // because we need to cache at least the first 2 values.
+            cache_size = 2; // because we need to cache at least the first 2 values.
         } else {
-            cache_size= _n + 1;
+            cache_size = _n + 1;
         }
-        uint[] memory cache= new uint[](cache_size);
-        uint[] memory result= new uint[](_n + 1);
-        cache[0]= 0;
-        cache[1]= 1;
+        uint256[] memory cache = new uint256[](cache_size);
+        uint256[] memory result = new uint256[](_n + 1);
+        cache[0] = 0;
+        cache[1] = 1;
 
-        for (uint i= 0; i < _n + 1; i++) {
+        for (uint256 i = 0; i < _n + 1; i++) {
             if (i > 1) {
                 cache[i] = cache[i - 1] + cache[i - 2];
             }
-            result[i]= cache[i];
+            result[i] = cache[i];
         }
 
         return result;
     }
 }
-
---
+++ test/fibonacci.t.sol
@@ -0,0 +1,191 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {Fibonacci} from "../src/Maths/fibonacci/fibonacci.sol";
+
+contract ContractTest is Test {
+    Fibonacci fib;
+
+    function setUp() public {
+        fib = new Fibonacci();
+    }
+
+    function testGoodFibRecursiveTerm() public {
+        // 35 - 40 is limit with foundry
+        assertEq(fib.fibRecursiveTerm(0), 0, "Fibonacci O-th term is 0.");
+        assertEq(fib.fibRecursiveTerm(1), 1, "Fibonacci 1-th term is 1.");
+        assertEq(fib.fibRecursiveTerm(2), 1, "Fibonacci 2-th term is 1.");
+        assertEq(fib.fibRecursiveTerm(3), 2, "Fibonacci 3-th term is 2.");
+        assertEq(fib.fibRecursiveTerm(4), 3, "Fibonacci 4-th term is 3.");
+        assertEq(fib.fibRecursiveTerm(5), 5, "Fibonacci 5-th term is 5.");
+        assertEq(fib.fibRecursiveTerm(6), 8, "Fibonacci 6-th term is 8.");
+        assertEq(fib.fibRecursiveTerm(7), 13, "Fibonacci 7-th term is 13.");
+        assertEq(fib.fibRecursiveTerm(8), 21, "Fibonacci 8-th term is 21.");
+        assertEq(fib.fibRecursiveTerm(9), 34, "Fibonacci 9-th term is 34.");
+        assertEq(fib.fibRecursiveTerm(10), 55, "Fibonacci 10-th term is 55.");
+        assertEq(
+            fib.fibRecursiveTerm(17),
+            1597,
+            "Fibonacci 17-th term is 1597."
+        );
+        //assertEq(fib.fibRecursiveTerm(99), 218922995834555169026, "Consume too much gas");
+    }
+
+    function testGoodFibRecursive() public {
+        uint256[] memory expected_total = new uint256[](11);
+        uint256[] memory result;
+
+        expected_total[0] = 0;
+        result = fib.fibRecursive(0);
+        assertEq(
+            expected_total[0],
+            result[0],
+            "Fibonacci 0-th suite last value is 0."
+        );
+
+        expected_total[1] = 1;
+        result = fib.fibRecursive(1);
+        assertEq(
+            expected_total[1],
+            result[1],
+            "Fibonacci 1-th suite last value is 1."
+        );
+
+        expected_total[2] = 1;
+        result = fib.fibRecursive(2);
+        assertEq(
+            expected_total[2],
+            result[2],
+            "Fibonacci 2-th suite last value is 1."
+        );
+
+        expected_total[3] = 2;
+        result = fib.fibRecursive(3);
+        assertEq(
+            expected_total[3],
+            result[3],
+            "Fibonacci 3-th suite last value is 2."
+        );
+
+        expected_total[4] = 3;
+        result = fib.fibRecursive(4);
+        assertEq(
+            expected_total[4],
+            result[4],
+            "Fibonacci 4-th suite last value is 3."
+        );
+
+        expected_total[5] = 5;
+        result = fib.fibRecursive(5);
+        assertEq(
+            expected_total[5],
+            result[5],
+            "Fibonacci 5-th suite last value is 5."
+        );
+
+        expected_total[6] = 8;
+        expected_total[7] = 13;
+        expected_total[8] = 21;
+        expected_total[9] = 34;
+        expected_total[10] = 55;
+
+        result = fib.fibRecursive(10);
+        for (uint256 i = 0; i <= 10; i++) {
+            assertEq(
+                result[i],
+                expected_total[i],
+                string(
+                    abi.encodePacked(
+                        "Fibonacci expected value for ",
+                        i,
+                        "-th is ",
+                        expected_total[i]
+                    )
+                )
+            );
+        }
+    }
+
+    function testGoodFibMemoization() public {
+        uint256[] memory expected_total = new uint256[](11);
+        uint256[] memory result;
+
+        expected_total[0] = 0;
+        result = fib.fibMemoization(0);
+        assertEq(
+            expected_total[0],
+            result[0],
+            "Fibonacci 0-th suite last value is 0."
+        );
+
+        expected_total[1] = 1;
+        result = fib.fibMemoization(1);
+        assertEq(
+            expected_total[1],
+            result[1],
+            "Fibonacci 1-th suite last value is 1."
+        );
+
+        expected_total[2] = 1;
+        result = fib.fibMemoization(2);
+        assertEq(
+            expected_total[2],
+            result[2],
+            "Fibonacci 2-th suite last value is 1."
+        );
+
+        expected_total[3] = 2;
+        result = fib.fibMemoization(3);
+        assertEq(
+            expected_total[3],
+            result[3],
+            "Fibonacci 3-th suite last value is 2."
+        );
+
+        expected_total[4] = 3;
+        result = fib.fibMemoization(4);
+        assertEq(
+            expected_total[4],
+            result[4],
+            "Fibonacci 4-th suite last value is 3."
+        );
+
+        expected_total[5] = 5;
+        result = fib.fibMemoization(5);
+        assertEq(
+            expected_total[5],
+            result[5],
+            "Fibonacci 5-th suite last value is 5."
+        );
+
+        expected_total[6] = 8;
+        expected_total[7] = 13;
+        expected_total[8] = 21;
+        expected_total[9] = 34;
+        expected_total[10] = 55;
+
+        result = fib.fibMemoization(10);
+        for (uint256 i = 0; i <= 10; i++) {
+            assertEq(
+                result[i],
+                expected_total[i],
+                string(
+                    abi.encodePacked(
+                        "Fibonacci expected value for ",
+                        i,
+                        "-th is ",
+                        expected_total[i]
+                    )
+                )
+            );
+        }
+
+        result = fib.fibMemoization(99);
+        assertEq(
+            result[99],
+            218922995834555169026,
+            "Fibonacci 99-th suite last value."
+        );
+    }
+}
