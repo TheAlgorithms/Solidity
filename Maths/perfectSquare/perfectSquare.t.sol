5cac0360468f5ffbc2bd15f3cbc46ad001621913
Perelyn-sama <okohebina@gmail.com>
2022-06-22 14:24:07 +0100

    added test for perfect square contract

---
+++ Maths/perfectSquare/perfectSquare.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "./perfectSquare.sol";
+
+contract PerfectSquareTest is Test {
+    // Target contract
+    PerfectSquare perfectSquare;
+
+    //  =====   Set up  =====
+    function setUp() public {
+        perfectSquare = new PerfectSquare();
+    }
+
+    /// @dev Test `perfectSquare`
+
+    function testSqrt() public {
+        uint256 num = 16;
+        bool isPerfectSquare = perfectSquare.perfectSquare(num);
+        assertEq(isPerfectSquare, true);
+    }
+}
