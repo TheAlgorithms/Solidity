e5c5da5d6be8cd89f228f76b82bb55aa71821a1c
Perelyn-sama <okohebina@gmail.com>
2022-06-25 20:19:39 +0100

    wrote addArray.sol test

---
+++ Maths/addArray/AddArray.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "./AddArray.sol";
+
+contract AddArrayTest is Test {
+    // Target contract
+    AddArray addArray;
+
+    /// Test Params
+    uint256[] public arr = [2, 8, 4, 5];
+
+    //  =====   Set up  =====
+    function setUp() public {
+        addArray = new AddArray();
+    }
+
+    /// @dev Test `addArr`
+
+    function test_addArr() public {
+        uint256 expected_result = 2 + 8 + 4 + 5;
+
+        uint256 result = addArray.addArr(arr);
+
+        assertEq(result, expected_result);
+    }
+}
