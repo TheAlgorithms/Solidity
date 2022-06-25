e895e171b7028fdbd561616b89354e1a6209f080
Perelyn-sama <okohebina@gmail.com>
2022-06-25 20:19:01 +0100

    wrote aliquot sum test

---
+++ Maths/aliquotSum/AliquotSum.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "./AliquotSum.sol";
+
+contract AliquotSumTest is Test {
+    // Target contract
+    AliquotSum aliquotSum;
+
+    //  =====   Set up  =====
+    function setUp() public {
+        aliquotSum = new AliquotSum();
+    }
+
+    /// @dev Test `getAliquotSum`
+
+    function test_getAliquotSum() public {
+        uint256 expected_result = 1 + 2 + 3;
+
+        uint256 result = aliquotSum.getAliquotSum(6);
+
+        assertEq(result, expected_result);
+    }
+}
