9699d6b340db241c9ebecf82948c7be750fa5909
Perelyn-sama <okohebina@gmail.com>
2022-07-04 20:23:59 +0100

    added test for MulArray.sol

--- src/Maths/MulArray.sol
+++ src/Maths/MulArray.sol
@@ -2,33 +2,33 @@
 pragma solidity ^0.8.0;
 
 /**
-* @title Array Multiplication.
-* @author Anthony (fps) https://github.com/fps8k.
-* @dev Contract to demonstrate how an array is multiplied.
-*/
+ * @title Array Multiplication.
+ * @author Anthony (fps) https://github.com/fps8k.
+ * @dev Contract to demonstrate how an array is multiplied.
+ */
 
-
-contract MulArray{
-    /// @dev Set the first array.
-    uint256[] public arr = [2, 8, 4, 5];
-
+contract MulArray {
     /**
-    * @dev Takes in an array of numbers and adds them and returns the cumultative product.
-    * @return product which is the cumultative product of the array.
-    */
-    function mulArr() public view returns(uint product){
+     * @dev Takes in an array of numbers and adds them and returns the cumultative product.
+     * @param _arr array of numbers
+     * @return product which is the cumultative product of the array.
+     */
+    function mulArr(uint256[] memory _arr)
+        public
+        pure
+        returns (uint256 product)
+    {
         /// @dev Initialize the total we need to return to 0.
-        uint _product = 1;
-        
+        uint256 _product = 1;
+
         /// @dev Loop through the array.
-        for (uint i = 0; i < arr.length; i++){
+        for (uint256 i = 0; i < _arr.length; i++) {
             /// @dev On every element, multiply the current _product with the value of the elementa.
             /// @dev For products > the maximum uint value, solidity versions > 0.8.0 will revert on overflow.
-            _product *= arr[i];
+            _product *= _arr[i];
         }
 
         /// @dev Return the product.
         product = _product;
     }
 }
-
