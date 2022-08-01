db5e9202fba08d0aec9636f95ab95cb8c6941a5c
Perelyn-sama <okohebina@gmail.com>
2022-08-01 14:29:31 +0100

    Added average mean and test

---
+++ src/Maths/AverageMean.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title find the mean value of an array of numbers
+ * @author Perelyn https://github.com/Perelyn-sama
+ * @dev https://en.wikipedia.org/wiki/Mean
+ */
+contract AverageMean {
+    /// find the mean value of an array of numbers
+    function getAverageMean(uint256[] memory _nums)
+        public
+        pure
+        returns (uint256 result)
+    {
+        uint256 sum;
+        for (uint256 i = 0; i < _nums.length; i++) {
+            sum += _nums[i];
+        }
+
+        result = sum / _nums.length;
+    }
+}
