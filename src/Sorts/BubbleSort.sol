2539c6e74aeaed8e6eb05407904a0f19d01971db
Perelyn-sama <okohebina@gmail.com>
2022-08-01 14:02:17 +0100

    Added Bubble sort and test

---
+++ src/Sorts/BubbleSort.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+
+/**
+ * @title Get the absolute value of an integer.
+ * @author Perelyn https://github.com/Perelyn-sama
+ * @dev https://www.khanacademy.org/math/cc-sixth-grade-math/cc-6th-negative-number-topic/cc-6th-absolute-value/v/absolute-value-of-integers
+ */
+contract BubbleSort is Test {
+    function bubbleSort(uint256[] memory _arr)
+        public
+        pure
+        returns (uint256[] memory sortedArr_)
+    {
+        uint256 length = _arr.length;
+        bool noSwaps;
+
+        for (uint256 i = length; i > 0; i--) {
+            // flag for optimization
+            noSwaps = true;
+            // Number of passes
+            for (uint256 j = 0; j < (i - 1); j++) {
+                // compare the adjacent positions
+                if (_arr[j] > _arr[j + 1]) {
+                    // swap the numbers
+                    uint256 lesser = _arr[j + 1];
+                    uint256 higher = _arr[j];
+                    _arr[j + 1] = higher;
+                    _arr[j] = lesser;
+                    noSwaps = false;
+                }
+            }
+            if (noSwaps) {
+                break;
+            }
+        }
+        sortedArr_ = _arr;
+    }
+}
---
+++ src/Sorts/test/BubbleSort.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "../BubbleSort.sol";
+
+contract BubbleSortTest is Test {
+    // Target contract
+    BubbleSort bubbleSort;
+
+    /// Test Params
+    uint256[] public arr = [65, 55, 45, 35, 25, 15, 10];
+    uint256[] expected_result = [10, 15, 25, 35, 45, 55, 65];
+
+    //  =====   Set up  =====
+    function setUp() public {
+        bubbleSort = new BubbleSort();
+    }
+
+    /// @dev Test `bubbleSort`
+
+    function test_bubbleSort() public {
+        uint256[] memory result = bubbleSort.bubbleSort(arr);
+        assertEq(expected_result, result);
+    }
+}
