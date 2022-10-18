7ff9d3d3610c490f2d4974e920aa9b4632ba4dc4
Sebastiano Faiella <xeno097.cp@gmail.com>
2022-10-17 23:03:40 -0400

    feat(Maths): completed BinaryExponentiation implementation but missing description

--- src/Maths/BinaryExponentiation.sol
+++ src/Maths/BinaryExponentiation.sol
@@ -1,4 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract BinaryExponentiation {}
+/// @title BinaryExponentiation
+/// @author https://github.com/xeno097
+/// @notice A contract that implements the binary exponentiation algorithm for signed and unsigned integers
+/// @dev TODO: Explain how the algorith works
+contract BinaryExponentiation {
+  /// @notice Takes two unsigned integers and calculates the base to the power of exp.
+  /// @param base the number which power must be calculated.
+  /// @param exp the exponent used to calculate the base power.
+  /// @return power the result of computing base to the power of exp.
+  function pow(uint256 base, uint256 exp) public pure returns (uint256) {
+    // Base case x**0 = 1
+    uint256 res = 1;
+
+    // Perform the left shift operation until exp is greater
+    // than 0.
+    for (; exp > 0; ) {
+      // Take the current least significant bit and verify that
+      // it is equal to 1, if it is multiply the current power
+      // of the base to the current result.
+      if (exp & 1 == 1) {
+        res *= base;
+      }
+      base *= base;
+      exp >>= 1;
+    }
+
+    return res;
+  }
+}
--- src/Maths/test/BinaryExponentiation.sol
+++ src/Maths/test/BinaryExponentiation.sol
@@ -5,9 +5,37 @@ import "forge-std/Test.sol";
 import "../BinaryExponentiation.sol";
 
 contract BinaryExponentiationTest is Test {
-    BinaryExponentiation binaryExponentiation;
+  // Target contract
+  BinaryExponentiation binaryExponentiation;
 
-    function setUp() public {
-        binaryExponentiation = new BinaryExponentiation();
-    }
+  //  =====   Set up  =====
+  function setUp() public {
+    binaryExponentiation = new BinaryExponentiation();
+  }
+
+  function testCalculatesBaseToThePowerOfExp(uint256 base, uint256 exp) public {
+    // Arrange
+    vm.assume(base != 0 && exp != 0);
+    // keep the base and exponent low not to cause an overflow.
+    vm.assume(base < 250 && exp < 32);
+
+    uint256 expectedRes = base**exp;
+
+    // Act
+    uint256 res = binaryExponentiation.pow(base, exp);
+
+    // Assert
+    assertEq(res, expectedRes);
+  }
+
+  function testRevertsIfResultOverflows(uint256 base, uint256 exp) public {
+    // Arrange
+    vm.assume(base > 250 && exp > 32);
+
+    // Assert
+    vm.expectRevert(stdError.arithmeticError);
+
+    // Act
+    binaryExponentiation.pow(base, exp);
+  }
 }
