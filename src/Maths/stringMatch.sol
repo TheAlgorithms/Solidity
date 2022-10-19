a10a7ee6bb2b8b82058cf492e88f47a8470e5f4a
Sreek9601 <sreekarsarvepalli9601@gmail.com>
2022-10-19 23:08:38 +0530

    Added stringMatching algorithm in Maths

---
+++ src/Maths/stringMatch.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+/**
+ * @title String Matching.
+ * @author Sreek  https://github.com/sreekar9601.
+ * @dev Contract to demonstrate matching of two strings.
+ */
+
+contract StringMatch {
+   
+    string firstString;
+string secondString;
+ 
+   function setFirstString(string memory _firstString) public{
+        firstString = _firstString;
+   }
+
+   function setSecondString(string memory _secondString) public{
+         secondString= _secondString;
+   }
+
+   function stringMatch()public view returns(bool){
+if (keccak256(abi.encodePacked(firstString)) == keccak256(abi.encodePacked(secondString))){
+    return true;
+}
+    return false;
+   }
+}
