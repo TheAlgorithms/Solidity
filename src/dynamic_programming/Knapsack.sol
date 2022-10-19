38252475a98b7b7e91eb2082ac47d3c59008c89c
Rohit <RohitSinghEmail@protonmail.com>
2022-10-19 20:11:43 +0530

    added dp alogorithm knapsack and tests

---
+++ src/dynamic_programming/Knapsack.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: UNLICENSED
+// https://atcoder.jp/contests/dp/tasks/dp_d
+// https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
+
+pragma solidity ^0.8.13;
+import "forge-std/console.sol";
+
+contract Knapsack {
+    mapping(uint256 => mapping(uint256 => uint256)) dp;
+
+    function knapsack(
+        uint256 numberOfItems,
+        uint256 totalWeight,
+        uint256[] memory weights,
+        uint256[] memory values
+    ) public returns (uint256) {
+        for (uint256 i = 0; i < numberOfItems; i++) {
+            for (uint256 j = 0; j <= totalWeight; j++) {
+                if (i == 0) {
+                    if (j >= weights[0]) {
+                        dp[i][j] = values[0];
+                        continue;
+                    }
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (j < weights[i]) {
+                    dp[i][j] = dp[i - 1][j] + 0;
+                    continue;
+                }
+
+                uint256 a = dp[i - 1][j - weights[i]] + values[i];
+                uint256 b = dp[i - 1][j];
+
+                dp[i][j] = a >= b ? a : b;
+            }
+        }
+
+        return dp[numberOfItems - 1][totalWeight];
+    }
+}
+
+contract KnapsackTestCases {
+    Knapsack knapsack;
+    uint256 n = 6;
+    uint256 w = 15;
+
+    uint256[] values = [5, 6, 4, 6, 5, 2];
+    uint256[] weights = [6, 5, 6, 6, 3, 7];
+
+    function test1() public returns (uint256) {
+        knapsack = new Knapsack();
+
+        return knapsack.knapsack(n, w, weights, values);
+    }
+}
---
+++ src/dynamic_programming/test/Knapsack.t.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "../Knapsack.sol";
+
+contract Knapsack1Test is Test {
+    Knapsack knapsack;
+
+    uint256 numberOfItems = 6;
+    uint256 totalWeight = 15;
+
+    uint256[] values = [5, 6, 4, 6, 5, 2];
+    uint256[] weights = [6, 5, 6, 6, 3, 7];
+
+    function setUp() public {
+        knapsack = new Knapsack();
+    }
+
+    function testKnapsack() public {
+        assertEq(
+            17,
+            knapsack.knapsack(numberOfItems, totalWeight, weights, values)
+        );
+    }
+}
+
+contract Knapsack2Test is Test {
+    Knapsack knapsack;
+
+    uint256 numberOfItems = 3;
+    uint256 totalWeight = 50;
+
+    uint256[] values = [60, 100, 120];
+    uint256[] weights = [10, 20, 30];
+
+    function setUp() public {
+        knapsack = new Knapsack();
+    }
+
+    function testKnapsack() public {
+        assertEq(
+            220,
+            knapsack.knapsack(numberOfItems, totalWeight, weights, values)
+        );
+    }
+}
+
+contract Knapsack3Test is Test {
+    Knapsack knapsack;
+
+    uint256 numberOfItems = 3;
+    uint256 totalWeight = 8;
+
+    uint256[] values = [30, 50, 60];
+    uint256[] weights = [3, 4, 5];
+
+    function setUp() public {
+        knapsack = new Knapsack();
+    }
+
+    function testKnapsack() public {
+        assertEq(
+            90,
+            knapsack.knapsack(numberOfItems, totalWeight, weights, values)
+        );
+    }
+}
+
+contract Knapsack4Test is Test {
+    Knapsack knapsack;
+
+    uint256 numberOfItems = 5;
+    uint256 totalWeight = 5;
+
+    uint256[] values = [
+        1000000000,
+        1000000000,
+        1000000000,
+        1000000000,
+        1000000000
+    ];
+    uint256[] weights = [1, 1, 1, 1, 1];
+
+    function setUp() public {
+        knapsack = new Knapsack();
+    }
+
+    function testKnapsack() public {
+        assertEq(
+            5000000000,
+            knapsack.knapsack(numberOfItems, totalWeight, weights, values)
+        );
+    }
+}
