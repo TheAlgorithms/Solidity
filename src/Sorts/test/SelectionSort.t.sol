69c944d702b14c8a18d7abf5def3d5f48cb80f38
Sumit Banik <36637433+iSumitBanik@users.noreply.github.com>
2022-10-21 14:44:09 +0530

    [test] Foundry Test for Selection sort (#55)

---
+++ src/Sorts/test/SelectionSort.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "../SelectionSort.sol";
+
+contract SelectionSortTest is Test {
+    // Target contract
+    SelectionSort selectionSort;
+
+    /// Test Params
+    uint256[] public arr = [12, 14, 11, 3, 15, 5, 4, 13, 7, 20];
+    uint256[] expected_result = [3, 4, 5, 7, 11, 12, 13, 14, 15, 20];
+
+    //  =====   Set up  =====
+    function setUp() public {
+        selectionSort = new SelectionSort();
+    }
+
+    /// @dev Test `selectionSort`
+
+    function test_SelectionSort() public {
+        uint256[] memory result = selectionSort.selectionSort(arr);
+        assertEq(expected_result, result);
+    }
+}
