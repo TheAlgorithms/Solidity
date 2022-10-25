53385d24bd06dee660f70a7fc712ad53dd6863c0
Perelyn <pisckid344@gmail.com>
2022-10-25 15:05:02 +0100

    updated pragma version from 0.6.6 to ^0.8.0

--- src/Maths/modulo.sol
+++ src/Maths/modulo.sol
@@ -1,5 +1,5 @@
 // Solidity program to demonstrate the Modulus operation
-pragma solidity ^0.6.6;
+pragma solidity ^0.8.0;
 
 // Creating a contract
 contract Modulo {
--- src/Maths/multiplyDigit.sol
+++ src/Maths/multiplyDigit.sol
@@ -1,5 +1,5 @@
 // Solidity program to demonstrate the multiplication
-pragma solidity 0.6.6;
+pragma solidity ^0.8.0;
 
 contract multiplyDigit {
     int128 firstNo;
